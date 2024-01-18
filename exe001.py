2f69325e986b784e3834faa89f855e21387184dd
EDSON CARLOS (Max) <121529646+mxzangado@users.noreply.github.com>
2024-01-18 16:47:03 -0300

    Add files via upload
    exe001  Olá  Mundo na tela.
    exe002 'print' no formato .fotmat
    desafio é codigo de uma soma

---
+++ exe001.py
@@ -0,0 +1,3 @@
+msg = 'Olá mundo'
+print(msg)
+print('Olá mundo')
---
+++ exe002.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome: ')
+#esse tipo de print é muito importante .format(nome)) formata o caractere para substitui o {}
+print('Seja bem vido, {}! '.format(nome))
